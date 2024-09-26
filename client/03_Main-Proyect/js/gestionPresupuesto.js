2c1526105007b3b4d45de0a9974c234398d33fee
Santiago Lorenzano <111286211+enderwigim@users.noreply.github.com>
2024-09-26 19:00:25 +0200

    class finished

--- client/03_Main-Proyect/js/gestionPresupuesto.js
+++ client/03_Main-Proyect/js/gestionPresupuesto.js
@@ -1,18 +1,32 @@
 // TODO: Crear las funciones, objetos y variables indicadas en el enunciado
 
-// TODO: Variable global
+let presupuesto = 0;
 
-
-function actualizarPresupuesto() {
+function actualizarPresupuesto(num) {
     // TODO
+    if (num < 0 || isNaN(num)) {
+        return -1;
+    }
+    presupuesto = num;
+    return presupuesto
 }
 
 function mostrarPresupuesto() {
-    // TODO
+    return `Tu presupuesto actual es de ${presupuesto} €`
 }
 
-function CrearGasto() {
-    // TODO
+function CrearGasto(num, descripcion) {
+    
+    if (num < 0 || isNaN(num)){
+        this.valor = 0;
+    } else {
+        this.valor = num;
+    }
+    this.descripcion = descripcion;
+
+    this.mostrarGasto = function (){
+        return `Gasto correspondiente a ${descripcion} con valor ${valor} €`
+    }
 }
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
