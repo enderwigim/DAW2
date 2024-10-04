6bee4b7fc6859f136d34a90046fb7f9e4dfee14e
Santiago Lorenzano <111286211+enderwigim@users.noreply.github.com>
2024-10-04 19:00:03 +0200

    Update gestionPresupuesto.js

--- client/03_Main-Proyect/js/gestionPresupuesto.js
+++ client/03_Main-Proyect/js/gestionPresupuesto.js
@@ -4,7 +4,6 @@ let presupuesto = 0;
 let idGasto = 0;
 let gastos = [];
 
-
 function actualizarPresupuesto(num) {
   // SI EL PRESUPUESTO ES MENOR A CERO O NO ES UN TIPO NUM
   // DEVUELVE -1
@@ -19,10 +18,10 @@ function mostrarPresupuesto() {
   //MOSTRAR PRESUPUESTO
   return `Tu presupuesto actual es de ${presupuesto} €`;
 }
-function listarGastos(){
+function listarGastos() {
   return gastos;
 }
-function anyadirGasto(newGasto){
+function anyadirGasto(newGasto) {
   // Agregamos el id a gasto.
   newGasto.id = idGasto;
   // Aumentamos el idGasto
@@ -30,20 +29,60 @@ function anyadirGasto(newGasto){
   // Añadimos a gastos el gasto seleccionado.
   gastos.push(newGasto);
 }
-function borrarGasto(id){
+function borrarGasto(id) {
   gastos = gastos.filter((gasto) => gasto.id !== id);
 }
-function calcularTotalGastos(){
+function calcularTotalGastos() {
   let total = 0;
-  
-  for (let i = 0; i < gastos.length; i++){
+
+  for (let i = 0; i < gastos.length; i++) {
     total += gastos[i].valor;
   }
   return total;
 }
-function calcularBalance(){
+function calcularBalance() {
   let balance = presupuesto - calcularTotalGastos();
   return balance;
+}
+
+function filtrarGastos({
+  fechaDesde,
+  fechaHasta,
+  valorMinimo,
+  valorMaximo,
+  descripcionContiene,
+  etiquetasTiene
+}) {
+  let filtered = [...gastos];
+  if (fechaDesde != undefined) {
+    filtered = filtered.filter(
+      (gasto) => Date.parse(fechaDesde) <= gasto.fecha
+    );
+  }
+  if (fechaHasta != undefined) {
+    filtered = filtered.filter(
+      (gasto) => Date.parse(fechaHasta) >= gasto.fecha
+    );
+  }
+  if (valorMinimo != undefined) {
+    filtered = filtered.filter((gasto) => valorMinimo < gasto.valor);
+  }
+  if (valorMaximo != undefined){
+    filtered = filtered.filter((gasto) => valorMaximo > gasto.valor);
+  }
+  if (descripcionContiene != undefined){
+    filtered = filtered.filter((gasto) => gasto.descripcion.toLowerCase().includes(descripcionContiene.toLowerCase()));
+  }
+  if (etiquetasTiene != undefined){
+    filtered = filtered.filter(producto => 
+      etiquetasTiene.some(etiqueta => producto.etiquetas.includes(etiqueta))
+    );
+  }
+  return filtered;
+}
+
+function agruparGastos(periodo, etiquetas, fechaDesde, fechaHasta ) {
+
 }
 
 // FUNCION CONSTRUCTORA DE GASTO
@@ -51,23 +90,27 @@ function calcularBalance(){
 function CrearGasto(descripcion, value, date, ...etiquetas) {
   // PROPIEDADES.
   // En caso de que el numero sea menor a 0 y/o no sea un numero. Seteamos valor a 0.
-  this.valor = (value < 0 || isNaN(value))?  0 : value;
+  this.valor = value < 0 || isNaN(value) ? 0 : value;
 
   // Seteamos descripcion
   this.descripcion = descripcion;
 
   this.fecha = isNaN(Date.parse(date)) ? Date.now() : Date.parse(date);
 
-  this.etiquetas = (etiquetas.length > 0) ? [...etiquetas] : [];
+  this.etiquetas = etiquetas.length > 0 ? [...etiquetas] : [];
 
   // METODOS
-  this.mostrarGastoCompleto = function() {
-   let etiquetasText = "";
-   for(let i = 0; i < this.etiquetas.length; i++){
-    etiquetasText += `- ${etiquetas[i]}\n`;
-   }
+  this.mostrarGastoCompleto = function () {
+    let etiquetasText = "";
+    for (let i = 0; i < this.etiquetas.length; i++) {
+      etiquetasText += `- ${etiquetas[i]}\n`;
+    }
 
-   return `Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €.\nFecha: ${new Date(this.fecha).toLocaleString()}\nEtiquetas:\n${etiquetasText}`;
+    return `Gasto correspondiente a ${this.descripcion} con valor ${
+      this.valor
+    } €.\nFecha: ${new Date(
+      this.fecha
+    ).toLocaleString()}\nEtiquetas:\n${etiquetasText}`;
   };
 
   this.mostrarGasto = function () {
@@ -81,20 +124,57 @@ function CrearGasto(descripcion, value, date, ...etiquetas) {
       this.valor = newValue;
     }
   };
-  this.actualizarFecha = function (date){
-    this.fecha = (isNaN(Date.parse(date)))? this.fecha : Date.parse(date);
-  }
-  this.anyadirEtiquetas = function (...nuevasEtiqueta){
-    let etiquetasAnyadir = nuevasEtiqueta.filter((etiqueta) => !this.etiquetas.includes(etiqueta));
+  this.actualizarFecha = function (date) {
+    this.fecha = isNaN(Date.parse(date)) ? this.fecha : Date.parse(date);
+  };
+  this.anyadirEtiquetas = function (...nuevasEtiqueta) {
+    let etiquetasAnyadir = nuevasEtiqueta.filter(
+      (etiqueta) => !this.etiquetas.includes(etiqueta)
+    );
     this.etiquetas = [...this.etiquetas, ...etiquetasAnyadir];
-  }
-  this.borrarEtiquetas = function(...etiquetasBorrar){
-    let etiquetasBorradas = etiquetas.filter((etiqueta) => !etiquetasBorrar.includes(etiqueta))
+  };
+  this.borrarEtiquetas = function (...etiquetasBorrar) {
+    let etiquetasBorradas = etiquetas.filter(
+      (etiqueta) => !etiquetasBorrar.includes(etiqueta)
+    );
     this.etiquetas = etiquetasBorradas;
-  }
+  };
+  this.obtenerPeriodoAgrupacion = function (datePeriod) {
+    // Obtenemos la fecha en objeto Date.
+    let dateDate = new Date(this.fecha);
+    let ret = "";
+
+    if (datePeriod == "anyo") {
+      ret = dateDate.getFullYear();
+    }
+    if (datePeriod == "mes") {
+      ret = `${dateDate.getFullYear()}-${(
+        "0" +
+        (dateDate.getMonth() + 1)
+      ).slice(-2)}`;
+    }
+    if (datePeriod == "dia") {
+      ret = `${dateDate.getFullYear()}-${(
+        "0" +
+        (dateDate.getMonth() + 1)
+      ).slice(-2)}-${("0" + dateDate.getDate()).slice(-2)}`;
+    }
+    return ret;
+  };
 }
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
-export { mostrarPresupuesto, actualizarPresupuesto, CrearGasto, listarGastos, anyadirGasto, borrarGasto, calcularTotalGastos, calcularBalance };
+export {
+  mostrarPresupuesto,
+  actualizarPresupuesto,
+  CrearGasto,
+  listarGastos,
+  anyadirGasto,
+  borrarGasto,
+  calcularTotalGastos,
+  calcularBalance,
+  filtrarGastos,
+  agruparGastos,
+};
