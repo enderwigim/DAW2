ec0605e526fdd428ed21392a6d06d0ab68429fb3
Santiago Lorenzano <111286211+enderwigim@users.noreply.github.com>
2024-12-12 17:13:48 +0100

    jueves - servidor

--- servidor/U8/ApiContactos/ApiContactos/Program.cs
+++ servidor/U8/ApiContactos/ApiContactos/Program.cs
@@ -1,8 +1,17 @@
+using ApiContactos.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+// Registrar el contexto de la base de datos 
+var connectionString = builder.Configuration.GetConnectionString("ApiContactos");
+builder.Services.AddDbContext<ApiContactosContexto>(options =>
+options.UseSqlServer(connectionString));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
