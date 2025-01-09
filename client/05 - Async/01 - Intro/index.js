c2d163642a4e9110bab8706765c037400ae62d66
Santiago Lorenzano <111286211+enderwigim@users.noreply.github.com>
2025-01-09 17:30:28 +0100

    iuhiu

--- client/05 - Async/01 - Intro/index.js
+++ client/05 - Async/01 - Intro/index.js
@@ -3,10 +3,11 @@
 var seconds = 0;
 var is_counting = false;
 var minutes = 0;
+var temp;
 
+let p = document.getElementById("temp");
 function temporizador() {
-    is_counting = -is_counting
-    let temp
+    is_counting = !is_counting;
     if (is_counting){
         temp = setInterval(() => {
     
@@ -14,7 +15,7 @@ function temporizador() {
             if (seconds === 60) {
                 minutes++
             }
-            console.log(`${minutes < 10? '0' + minutes : minutes}:${seconds < 10? '0' + seconds : seconds }`); 
+            p.innerHTML = (`${minutes < 10? '0' + minutes : minutes}:${seconds < 10? '0' + seconds : seconds }`); 
         },1000)
     } else {
         clearInterval(temp)
@@ -23,11 +24,12 @@ function temporizador() {
 
 
 let initbtn = document.getElementById("inicio");
-initbtn.addEventListener("click", temporizador())
+initbtn.addEventListener("click", temporizador)
 
 let resetbtn = document.getElementById("reset");
 resetbtn.addEventListener("click", () => {
-    is_counting = false;
     seconds = 0;
     minutes = 0;
+    p.innerHTML = `00:00`
+    clearInterval(temp)
 })
