60f100e59f403656c73eeff9f94562746d31b485
Santiago Lorenzano <111286211+enderwigim@users.noreply.github.com>
2025-01-14 20:31:24 +0100

    now it works. at least

---
+++ servidor/Proyecto Final/MiluTienda/MiluTienda/Migrations/20250114193052_Inicial.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MiluTienda.Migrations
+{
+    /// <inheritdoc />
+    public partial class Inicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categorias",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Descripcion = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categorias", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Clientes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Telefono = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Direccion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Poblacion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CodPostal = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Nif = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clientes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Estados",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Descripcion = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Estados", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FamiliaProductos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Marca = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Imagen = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CategoriaId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FamiliaProductos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_FamiliaProductos_Categorias_CategoriaId",
+                        column: x => x.CategoriaId,
+                        principalTable: "Categorias",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pedidos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FechaCreacion = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Confirmado = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Preparado = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Enviado = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Cobrado = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Devuelto = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Anulado = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    ClienteId = table.Column<int>(type: "int", nullable: false),
+                    EstadoId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pedidos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Pedidos_Clientes_ClienteId",
+                        column: x => x.ClienteId,
+                        principalTable: "Clientes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Pedidos_Estados_EstadoId",
+                        column: x => x.EstadoId,
+                        principalTable: "Estados",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Productos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Atributo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Descripcion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Precio = table.Column<decimal>(type: "decimal(9,2)", nullable: false),
+                    PrecioCadena = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Stock = table.Column<int>(type: "int", nullable: false),
+                    Imagen = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Escaparate = table.Column<bool>(type: "bit", nullable: false),
+                    FamiliaProductoId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Productos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Productos_FamiliaProductos_FamiliaProductoId",
+                        column: x => x.FamiliaProductoId,
+                        principalTable: "FamiliaProductos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LineasPedido",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Cantidad = table.Column<int>(type: "int", nullable: false),
+                    Precio = table.Column<decimal>(type: "decimal(11,2)", nullable: true),
+                    Descuento = table.Column<decimal>(type: "decimal(2,0)", nullable: true),
+                    PedidoId = table.Column<int>(type: "int", nullable: true),
+                    ProductoId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LineasPedido", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_LineasPedido_Pedidos_PedidoId",
+                        column: x => x.PedidoId,
+                        principalTable: "Pedidos",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_LineasPedido_Productos_ProductoId",
+                        column: x => x.ProductoId,
+                        principalTable: "Productos",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FamiliaProductos_CategoriaId",
+                table: "FamiliaProductos",
+                column: "CategoriaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LineasPedido_PedidoId",
+                table: "LineasPedido",
+                column: "PedidoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LineasPedido_ProductoId",
+                table: "LineasPedido",
+                column: "ProductoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pedidos_ClienteId",
+                table: "Pedidos",
+                column: "ClienteId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pedidos_EstadoId",
+                table: "Pedidos",
+                column: "EstadoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Productos_FamiliaProductoId",
+                table: "Productos",
+                column: "FamiliaProductoId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LineasPedido");
+
+            migrationBuilder.DropTable(
+                name: "Pedidos");
+
+            migrationBuilder.DropTable(
+                name: "Productos");
+
+            migrationBuilder.DropTable(
+                name: "Clientes");
+
+            migrationBuilder.DropTable(
+                name: "Estados");
+
+            migrationBuilder.DropTable(
+                name: "FamiliaProductos");
+
+            migrationBuilder.DropTable(
+                name: "Categorias");
+        }
+    }
+}
--- servidor/Proyecto Final/MiluTienda/MiluTienda/Models/Producto.cs
+++ servidor/Proyecto Final/MiluTienda/MiluTienda/Models/Producto.cs
@@ -6,18 +6,20 @@ namespace MiluTienda.Models
     public class Producto
     {
         public int Id { get; set; }
-        [Display(Name = "Familia Origen")]
-        public int FamiliaProductoId { get; set; }
-
-        public FamiliaProducto? FamiliaProducto { get; set; }
+        
         public string? Atributo { get; set; }
 
         [Display(Name = "Nombre")]
-        public string? NombreVariante { get; set; }
+        [Required]
+        public string Nombre { get; set; }
+
+        [Display(Name = "Descripción")]
+        [Required]
+        public string Descripcion { get; set; }
 
         [DataType(DataType.Currency)]
         [Column(TypeName = "decimal(9, 2)")]
-        public decimal PrecioVariante { get; set; }
+        public decimal Precio { get; set; }
 
         [Display(Name = "Precio")]
         [RegularExpression(@"^[-0123456789]+[0-9.,]*$",
@@ -27,18 +29,23 @@ namespace MiluTienda.Models
         {
             get
             {
-                return Convert.ToString(PrecioVariante).Replace(',', '.');
+                return Convert.ToString(Precio).Replace(',', '.');
             }
             set
             {
-                PrecioVariante = Convert.ToDecimal(value.Replace('.', ','));
+                Precio = Convert.ToDecimal(value.Replace('.', ','));
             }
         }
 
         public int Stock { get; set; } = 0;
         public string? Imagen { get; set; }
 
+        public bool Escaparate { get; set; }
+
         public ICollection<LineaPedido>? LineasPedido { get; set; }
+        public int FamiliaProductoId { get; set; }
+        [Display(Name = "Familia Origen")]
+        public FamiliaProducto? FamiliaProducto { get; set; }
 
     }
 }
