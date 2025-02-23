46ad2d18b85428ca378751214ea0f5e0afe631a6
Santiago Lorenzano <enderwigim@gmail.com>
2025-02-23 13:58:16 +0100

    asda

--- servidor/Proyecto Final/MiluTienda/MiluTienda/Controllers/UsuariosController.cs
+++ servidor/Proyecto Final/MiluTienda/MiluTienda/Controllers/UsuariosController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.V4.Pages.Account.Internal;
 using Microsoft.AspNetCore.Mvc;
 using MiluTienda.Data;
+using MiluTienda.Models;
 
 namespace MiluTienda.Controllers
 {
@@ -8,10 +11,12 @@ namespace MiluTienda.Controllers
     public class UsuariosController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
 
-        public UsuariosController(ApplicationDbContext context)
+        public UsuariosController(ApplicationDbContext context, UserManager<IdentityUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
         public IActionResult Index()
@@ -28,6 +33,34 @@ namespace MiluTienda.Controllers
 
             return View(usuarios.ToList());
         }
+        // GET: Usuarios/Create 
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Usuarios/Create 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Email,Password")]
+            RegisterModel.InputModel model)
+        {
+            // Se crea el nuevo usuario 
+            var user = new IdentityUser();
+            user.UserName = model.Email;
+            user.Email = model.Email;
+            string usuarioPWD = model.Password;
+            var result = await _userManager.CreateAsync(user, usuarioPWD);
+
+            // Se asigna el rol de "Administrador" al usuario 
+            if (result.Succeeded)
+            {
+                var result1 = await _userManager.AddToRoleAsync(user, "Administrador");
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(model);
+        }
     }
 } 
-}
+
--- servidor/Proyecto Final/MiluTienda/MiluTienda/Models/UsuariosConRol.cs
+++ servidor/Proyecto Final/MiluTienda/MiluTienda/Models/UsuariosConRol.cs
@@ -2,5 +2,8 @@
 {
     public class UsuariosConRol
     {
+    public string? Email { get; set; }
+    public string? NombreUsuario { get; set; }
+    public string? RolDeUsuario { get; set; }
     }
 }
